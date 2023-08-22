52bfbb064b4a3b420f931aad2816f475d9926ad4
18050413009 <159951asd>
2023-08-22 22:18:15 +0800

    go

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,27 @@
+﻿class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> result;
+        vector<int> combination;
+        sort(candidates.begin(), candidates.end());
+        dfs(candidates, target, 0, combination, result);
+        return result;
+    }
+
+private:
+    void dfs(vector<int>& candidates, int target, int start, vector<int>& combination, vector<vector<int>>& result) {
+        if (target == 0) {
+            result.push_back(combination);
+            return;
+        }
+
+        for (int i = start; i < candidates.size(); i++) {
+            if (candidates[i] > target) {
+                break;
+            }
+            combination.push_back(candidates[i]);
+            dfs(candidates, target - candidates[i], i, combination, result);
+            combination.pop_back();
+        }
+    }
+};
